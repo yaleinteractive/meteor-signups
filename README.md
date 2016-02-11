56656778dc1423f922a4962d538b719f1230f276
Dan Michaelson <dan@linkedbyair.net>
2016-02-11 22:27:20 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+To run the app in Cloud9, open a terminal and type:
+   
+    meteor --port $IP:$PORT
+    
+Then after it boots, click the URL next to "App running at", and do "Open".
+
+In the new browser tab that opens, you can copy the URL and share it with others.
+The app will normally remain running until there has been one hour of inactivity.
+
+If you need to stop the app, in that same Terminal tab press `Ctrl-C`.
+But you shouldn't need to stop it since changes are reloaded automatically.
+
+If you want to access the database directly, in a new terminal type:
+
+    meteor mongo
+    
+Then at the MongoDB prompt you can create a record:
+
+    db.signups.insert({name: "Dan", color: "blue"});
+    
+Or see all records:
+
+    db.signups.find();
+    
+Remember that `signups` is the name of our collection; you may name your collection
+differently.
+
+Meteor tutorial: https://www.meteor.com/tutorials/blaze/creating-an-app
+
+Meteor documentation: http://docs.meteor.com/
+
---
+++ signup.js
@@ -0,0 +1,74 @@
+// This code executes on the client and on the server
+
+// Declare a collection in the Mongo database
+var Signups = new Mongo.Collection("signups");
+
+
+// This code executes only on the client
+
+if (Meteor.isClient) {
+  
+  Template.body.helpers({
+     
+    // The signups helper returns a list of the signups.
+    // Find all the signups in the database and return them.
+    signups: function() {
+        return Signups.find({}, {sort: {time: 1}});  
+    }
+     
+  }); 
+
+  Template.signup.events({
+    
+    // This function is called whenever there is a click
+    // event on a delete link in the "signup" template
+    "click .delete": function(event) {
+      
+      // Tell the browser not to do its default behavior 
+      // (which would reload the page)
+      event.preventDefault();
+
+      // Using the Mongo id of this template's object, tell Mongo
+      // to remove the object from the database
+      Signups.remove(this._id);
+      
+    }
+    
+  });
+
+  Template.new.events({
+    
+    // This function is called whenever there is a submit
+    // event in the "new" template
+    "submit": function(event) {
+      
+      // Tell the browser not to do its default behavior 
+      // (which would reload the page)
+      event.preventDefault();
+      
+      // Get the <form> HTML element (which by definition is
+      // the target of the submit event)
+      var form = event.target;
+      
+      // Insert a signup into the database collection
+      Signups.insert({
+        name: form.name.value,
+        color: form.color.value,
+        github: form.github.value,
+        time: form.time.value,
+      });
+      
+      // Clear the text fields
+      form.name.value = '';
+      form.color.value = '';
+      form.github.value = '';
+      form.time.value = '';
+
+      // Focus the name field
+      form.name.focus();
+      
+    }
+    
+  });
+  
+}
